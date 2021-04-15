62b299fd0d891729cb42471ed0508c22a4e12d33
GangHo <tiger452003@gmail.com>
2021-04-15 23:48:49 +0900

    spring interceptor 등록

---
+++ src/main/java/termi/termispring/config/WebConfig.java
@@ -0,0 +1,16 @@
+package termi.termispring.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import termi.termispring.interceptor.LoginInterceptor;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginInterceptor()).addPathPatterns("/*");
+    }
+}
---
+++ src/main/java/termi/termispring/interceptor/LoginInterceptor.java
@@ -0,0 +1,15 @@
+package termi.termispring.interceptor;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LoginInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        System.out.println("interceptor test");
+        return true;
+    }
+}
