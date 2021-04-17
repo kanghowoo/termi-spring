dc738f2168622113e710edbcc141e0544af5e0e9
GangHo <tiger452003@gmail.com>
2021-04-18 04:41:35 +0900

    interceptor에서 Authorization token값 확인

--- src/main/java/termi/termispring/interceptor/LoginInterceptor.java
+++ src/main/java/termi/termispring/interceptor/LoginInterceptor.java
@@ -9,7 +9,16 @@ import javax.servlet.http.HttpServletResponse;
 public class LoginInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        System.out.println("interceptor test");
+        String token = request.getHeader("Authorization");
+
+
+        if(token==null) {
+            System.out.println("token null"); // <-로그인 필요한 화면으로 이동
+
+        }
+        else {
+            System.out.println("token test : " +token);
+        }
         return true;
     }
 }
