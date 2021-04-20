0c0890b5006d0063d640c2e181385c28144467af
GangHo <tiger452003@gmail.com>
2021-04-20 23:42:02 +0900

    Interceptor에서 service layer 호출(의존성 주입)이 안되는 오류 fix

--- src/main/java/termi/termispring/config/WebConfig.java
+++ src/main/java/termi/termispring/config/WebConfig.java
@@ -1,6 +1,7 @@
 package termi.termispring.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -11,6 +12,11 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new LoginInterceptor()).addPathPatterns("/*");
+        registry.addInterceptor(loginInterceptor()).addPathPatterns("/**");
+    }
+
+    @Bean
+    public LoginInterceptor loginInterceptor() {
+        return new LoginInterceptor();
     }
 }
